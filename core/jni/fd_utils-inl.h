89b3f3c5f933d376e2b17f649dbcce6a4272872b
Shivom007 <yashpaljoshi76@gmail.com>
2018-07-28 14:16:10 +0000

    mtk_fixes
    Change-Id: I45edf09faab9848abfaad31c9af9d96365d3d42d

--- core/jni/fd_utils-inl.h
+++ core/jni/fd_utils-inl.h
@@ -58,6 +58,7 @@ static const char* kPathWhitelist[] = {
   "/dev/urandom",
   "/dev/ion",
   "/dev/dri/renderD129", // Fixes b/31172436
+  "/proc/ged", // MTK add
   "/system/framework/org.cyanogenmod.platform-res.apk",
 #ifdef PATH_WHITELIST_EXTRA_H
 PATH_WHITELIST_EXTRA_H
